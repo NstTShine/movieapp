dbad3fc2690670e505be7daffc67a405a1a4460d
TShine <tuan.tta1991@gmail.com>
2016-10-28 09:10:36 +0700

    signin signup using devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027064732) do
+ActiveRecord::Schema.define(version: 20161027160442) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20161027064732) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "name"
-    t.string   "film_lenght"
+    t.string   "film_length"
     t.string   "director"
     t.text     "description"
     t.string   "image"
@@ -55,10 +55,21 @@ ActiveRecord::Schema.define(version: 20161027064732) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "email"
     t.integer  "role"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
